05fa306ffd4ffac45ad65b268ce1619685a1d4e1
refgd <refgdgn@qq.com>
2026-05-11 11:43:44 -0400

    添加布局

--- app/src/main/java/io/legado/app/help/source/BookSourceExtensions.kt
+++ app/src/main/java/io/legado/app/help/source/BookSourceExtensions.kt
@@ -6,7 +6,6 @@ import io.legado.app.constant.BookType
 import io.legado.app.data.entities.BookSource
 import io.legado.app.data.entities.BookSourcePart
 import io.legado.app.data.entities.rule.ExploreKind
-import io.legado.app.model.Debug
 import io.legado.app.ui.main.explore.ExploreAdapter.Companion.exploreInfoMapList
 import io.legado.app.utils.ACache
 import io.legado.app.utils.GSON
@@ -104,7 +103,7 @@ suspend fun BookSource.exploreKinds(): List<ExploreKind> {
                     }
                     else -> exploreUrl
                 }
-                Debug.log("refgd", ruleStr)
+                
                 if (!ruleStr.isValidExploreKindsRule()) {
                     return@runCatching
                 }
